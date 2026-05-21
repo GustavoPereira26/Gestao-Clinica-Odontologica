0759e3f2cd08bf2913614392112b86447c1d2f8e
joão <joaocarrilholoza@gmail.com>
2026-05-21 20:43:50 -0300

    .

---
+++ dentus-clinic/backend/DentusClinic.API/Migrations/20260413020041_AjustesTipos.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentusClinic.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustesTipos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TipoAcesso",
+                table: "Logins",
+                type: "int",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Logins",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LoginId",
+                table: "Funcionarios",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Dentistas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DataNascimento",
+                table: "Dentistas",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<long>(
+                name: "LoginId",
+                table: "Dentistas",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoginId",
+                table: "Funcionarios");
+
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "Dentistas");
+
+            migrationBuilder.DropColumn(
+                name: "LoginId",
+                table: "Dentistas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TipoAcesso",
+                table: "Logins",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Senha",
+                table: "Logins",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Dentistas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ dentus-clinic/backend/DentusClinic.API/Migrations/20260517010207_PlanosServicoOpcional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentusClinic.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlanosServicoOpcional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IdServico",
+                table: "Planos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IdServico",
+                table: "Planos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
